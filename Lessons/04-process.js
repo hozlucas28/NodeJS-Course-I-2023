20ef2b8940c43972f12080df79a2f2c6b9a63cf4
hozlucas28 <hozlucas28@gmail.com>
2023-08-09 00:23:53 -0300

    Upload fourth lesson

---
+++ Lessons/04-process.js
@@ -0,0 +1,13 @@
+/* --------------------------------------------------------------------------
+ * APUNTES:
+ * 		   Process otorga información relacionada con el proceso actual.
+-------------------------------------------------------------------------- */
+
+// Argumentos
+console.log(process.argv)
+
+// Variables de entorno
+console.log(process.env.MY_CUSTOM_VARIABLE)
+
+// Ruta (path) desde donde se esta ejecutando el archivo
+console.log(process.cwd)
